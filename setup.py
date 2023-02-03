38363691b9d04d922b9d44aa282be8bbfdfb120c
Vadim <explosive.va@gmail.com>
2023-02-03 21:14:18 +0500

    50% - 03.02.23 build (c)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ APP = ['app.py']
 DATA_FILES = ['512png.png', 'logo.png']
 OPTIONS = {
     'iconfile': '512png.png',
-    'argv_emulation': False,
+    # 'argv_emulation': True,
     'plist': {
         'CFBundleName': APP_NAME,
         'CFBundleDisplayName': APP_NAME,
